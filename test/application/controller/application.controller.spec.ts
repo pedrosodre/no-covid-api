d2bdf891032fdb8d591ad093a18c5d2dbdeb6e3e
Pedro Sodré <pedro@pedrosodre.dev>
2020-04-07 00:45:12 -0300

    Add unit tests for application controller

--- test/application/controller/application.controller.spec.ts
+++ test/application/controller/application.controller.spec.ts
@@ -1,12 +1,15 @@
 import { ApplicationController } from '../../../src/application/controller/application.controller';
 
+const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUGVkcm8gU29kcsOpIiwibWVzc2FnZSI6Ik9sw6EsIGN1cmlvc28gOikiLCJpYXQiOjE1MTYyMzkwMjJ9.JfuytMqdMxo35jsjwxSv3L99dxWqz_vf3HEe-gKCmIA';
 describe('ApplicationController', () => {
     let applicationController: ApplicationController;
     let applicationService;
 
     beforeAll(() => {
         applicationService = {
-            setApplication: jest.fn()
+            setApplication: jest.fn(),
+            authorize: jest.fn(),
+            blacklistJwt: jest.fn(),
         };
     });
 
@@ -14,7 +17,7 @@ describe('ApplicationController', () => {
         applicationController = new ApplicationController(applicationService);
     });
 
-    it('should return "201 - Created"', async () => {
+    it('createApplication() should return "201 - Created"', async () => {
         const application = {
             id: 'example-app',
             key: 'appKey',
@@ -38,5 +41,37 @@ describe('ApplicationController', () => {
         expect(response.statusCode).toBe(201);
         expect(response.message).toBe('Created');
         expect(response.application).toMatchObject(application);
+    });
+
+    it('generateToken() should return "200 - OK"', async () => {
+        applicationService.authorize.mockImplementation(() => jwt);
+
+        const response = await applicationController.generateToken(
+            {
+                application: {
+                    id: 'example-app'
+                }
+            }
+        );
+
+        expect(response.statusCode).toBe(200);
+        expect(response.message).toBe('OK');
+        expect(response.application.jwt).toBe(jwt);
+    });
+
+    it('patchJwtToBlacklist() should return "200 - OK"', async () => {
+        applicationService.blacklistJwt.mockImplementation();
+
+        const response = await applicationController.patchJwtToBlacklist(
+            {
+                application: {
+                    id: 'example-app'
+                }
+            },
+            { jwt }
+        );
+
+        expect(response.statusCode).toBe(200);
+        expect(response.message).toBe('OK');
     });
 });
